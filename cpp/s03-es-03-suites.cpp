8bc4df4b5f27805ec7c09ced4c06a679e553aa81
jnf611 <jnf611@gmail.com>
2016-02-24 18:01:48 +0100

    [DEV] S03 ES03 subject
    Change-Id: I4234c1de29377ef9ecac3c18f69230eade7ac427

---
+++ cpp/s03-es-03-suites.cpp
@@ -0,0 +1,34 @@
+/* semaine 03, exercice supplémentaire 03
+Écrivez un programme qui calcule les 10 premiers termes de la suite Un telle que:
+                      Un
+U0  =   1,   Un+1  = -----
+                      n+1
+Vous devez trouver:
+
+U0  =   1
+U1  =   1
+U2  =   0.5
+U3  =   0.1666667
+U4  =   0.0416667
+U5  =   0.0083333
+U6  =   0.0013889
+U7  =   0.000198413
+U8  =   2.48016e-5
+U9  =   2.75573e-06
+U10  =   2.75573e-07
+
+b) Modifiez votre programme pour qu'il calcule simultanément la suite Un et la série Vn, où
+        n
+Vn  =   ∑   Ui
+      i = 0
+
+Vérifiez que Vn converge vers e = exp(1) = 2.71828...*/
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	return 0;
+}
+ 
